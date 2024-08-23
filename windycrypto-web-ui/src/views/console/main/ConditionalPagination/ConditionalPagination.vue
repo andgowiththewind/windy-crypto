d0d224b86820fe87a16abb8a5c2052f1664fc84e
gust <2414566866@qq.com>
2024-08-23 12:03:32 +0800

    web-ui 条件分页查询

--- windycrypto-web-ui/src/views/console/main/ConditionalPagination/ConditionalPagination.vue
+++ windycrypto-web-ui/src/views/console/main/ConditionalPagination/ConditionalPagination.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    <h1>ConditionalPagination</h1>
+  <div v-loading="loading">
+    <div style="text-align: right;">
+    </div>
+    <div></div>
   </div>
 </template>
 
@@ -13,7 +15,14 @@ export default {
   name: "ConditionalPagination",
   components: {},
   data() {
-    return {}
+    return {
+      loading: false,
+      payload: {
+        model: {name: ''},
+        page: {pageNum: 1, pageSize: 10, total: 0},
+        params: {scope: 'all', path: process.env.VUE_APP_DEV_TEST_FOLDER_PATH || ''},
+      },
+    }
   },// data
   methods: {},// methods
   watch: {
