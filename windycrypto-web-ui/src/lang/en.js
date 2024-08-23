6fb06821f9fe2ad47e2527003aa4bdf533d93628
gust <2414566866@qq.com>
2024-08-23 14:10:51 +0800

    web-ui ConditionalPagination

--- windycrypto-web-ui/src/lang/en.js
+++ windycrypto-web-ui/src/lang/en.js
@@ -1,4 +1,12 @@
 export default {
-    i18n_1826858923819405312:'please input the path',
-    i18n_1826859557616488448:'please enter the desired top-level directory path and press Enter...',
+    i18n_1826858923819405312: 'please input the path',
+    i18n_1826859557616488448: 'please enter the desired top-level directory path and press Enter...',
+    i18n_1826861420642439168: 'query',
+    i18n_1826862190838288384: 'NotEncrypted',
+    i18n_1826862378374008832: 'Encrypted',
+    i18n_1826862378374008833: 'All',
+    i18n_1826863045788438528: 'filename',
+    i18n_1826863045788438529: 'filename supports fuzzy query',
+    i18n_1826863045788438530: 'query file list',
+    i18n_1826863045788438531: 'reset',
 }
--- windycrypto-web-ui/src/views/console/main/ConditionalPagination/ConditionalPagination.vue
+++ windycrypto-web-ui/src/views/console/main/ConditionalPagination/ConditionalPagination.vue
@@ -1,6 +1,24 @@
 <template>
   <div v-loading="loading">
     <div style="text-align: right;">
+      <el-form @submit.native.prevent size="mini" :inline="true" :model="payload" ref="payloadRef">
+        <el-form-item :label="$t('i18n_1826861420642439168')" prop="payloadParamsScope">
+          <el-radio-group v-model="payload.params.scope">
+            <el-radio-button label="notEncrypted">{{ $t('i18n_1826862190838288384') }}</el-radio-button>
+            <el-radio-button label="encrypted">{{ $t('i18n_1826862378374008832') }}</el-radio-button>
+            <el-radio-button label="all">{{ $t('i18n_1826862378374008833') }}</el-radio-button>
+          </el-radio-group>
+        </el-form-item>
+        <el-form-item :label="$t('i18n_1826863045788438528')" prop="payloadModelName">
+          <el-input :style="{minWidth:'215px'}" v-model="payload.model.name" :placeholder="$t('i18n_1826863045788438529')"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button-group>
+            <el-button type="primary" icon="el-icon-arrow-left" style="width: 15vw;" @click="submitConditionPagingQuery">{{ $t('i18n_1826863045788438530') }}</el-button>
+            <el-button type="primary" plain style="width: 5vw;" @click="resetConditionalForm">{{ $t('i18n_1826863045788438531') }}<i class="el-icon-arrow-right el-icon--right"></i></el-button>
+          </el-button-group>
+        </el-form-item>
+      </el-form>
     </div>
     <div></div>
   </div>
@@ -24,7 +42,12 @@ export default {
       },
     }
   },// data
-  methods: {},// methods
+  methods: {
+    resetConditionalForm() {
+    },
+    submitConditionPagingQuery() {
+    },
+  },// methods
   watch: {
     // 'searchParamVo.topPath': {handler: function (val, oldVal) {if (val) {this.searchParamVo.topPath = val;this.searchParamVo.topPath = '';}}, deep: true},
   },// watch
