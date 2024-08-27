8744dc61168e0a95f4e4d97e70107be7605a1553
gust <2414566866@qq.com>
2024-08-27 15:01:48 +0800

    crypto

--- src/main/java/com/gust/cafe/windycrypto/service/InsightTableService.java
+++ src/main/java/com/gust/cafe/windycrypto/service/InsightTableService.java
@@ -27,8 +27,6 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
-import java.util.function.Function;
-import java.util.function.Predicate;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
