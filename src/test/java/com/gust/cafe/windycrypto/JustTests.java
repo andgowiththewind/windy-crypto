51a1955810798426c2b8032521aa04c1e192ad95
gust <2414566866@qq.com>
2024-08-27 20:53:09 +0800

    crypto

--- src/test/java/com/gust/cafe/windycrypto/JustTests.java
+++ src/test/java/com/gust/cafe/windycrypto/JustTests.java
@@ -1,4 +1,15 @@
 package com.gust.cafe.windycrypto;
 
+import cn.hutool.core.collection.ListUtil;
+import cn.hutool.crypto.digest.DigestUtil;
+import com.gust.cafe.windycrypto.util.AesUtils;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
 public class JustTests {
+
+    @Test
+    public void test01() {
+    }
 }
