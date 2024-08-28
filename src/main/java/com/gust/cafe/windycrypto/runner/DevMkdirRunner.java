fc93325c6c3a98ed37ada9e85626b8343eb0f08a
gust <2414566866@qq.com>
2024-08-29 01:02:42 +0800

    crypto check

--- src/main/java/com/gust/cafe/windycrypto/runner/DevMkdirRunner.java
+++ src/main/java/com/gust/cafe/windycrypto/runner/DevMkdirRunner.java
@@ -5,6 +5,7 @@ import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.system.SystemUtil;
 import lombok.extern.slf4j.Slf4j;
+import net.bytebuddy.dynamic.scaffold.TypeWriter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -12,6 +13,7 @@ import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
+import java.io.IOException;
 
 /**
  * 测试环境创建目录
@@ -35,24 +37,36 @@ public class DevMkdirRunner implements ApplicationRunner {
         if (StrUtil.equalsIgnoreCase(springProfilesActive, "dev")) {
             String currentDir = SystemUtil.getUserInfo().getCurrentDir();
             File file = FileUtil.file(currentDir, "target", "测试被加解密顶级目录");
-            for (int i = 0; i < 1; i++) {
-                int numI = i + 1;
-                String nameI = StrUtil.fillAfter(Convert.toStr(numI), '0', 4);
-                File levelOne = FileUtil.file(file, nameI);
-                FileUtil.mkdir(levelOne);
-                for (int j = 0; j < 100; j++) {
-                    int numJ = j + 1;
-                    String nameJ = StrUtil.fillAfter(Convert.toStr(numJ), '0', 4);
-                    String levelTwoName = StrUtil.format("{}-{}", nameI, nameJ);
-                    File levelTwo = FileUtil.file(levelOne, levelTwoName);
-                    FileUtil.mkdir(levelTwo);
-                    // 写入一个文件
-                    File txt = FileUtil.file(levelTwo, StrUtil.format("{}.txt", levelTwoName));
-                    txt.createNewFile();
-                    FileUtil.writeUtf8String(levelTwoName, txt);
-                }
-            }
+            // type01(file);
+            type02(file);
         }
 
+    }
+
+    private static void type01(File file) throws IOException {
+        for (int i = 0; i < 1; i++) {
+            int numI = i + 1;
+            String nameI = StrUtil.fillAfter(Convert.toStr(numI), '0', 4);
+            File levelOne = FileUtil.file(file, nameI);
+            FileUtil.mkdir(levelOne);
+            for (int j = 0; j < 100; j++) {
+                int numJ = j + 1;
+                String nameJ = StrUtil.fillAfter(Convert.toStr(numJ), '0', 4);
+                String levelTwoName = StrUtil.format("{}-{}", nameI, nameJ);
+                File levelTwo = FileUtil.file(levelOne, levelTwoName);
+                FileUtil.mkdir(levelTwo);
+                // 写入一个文件
+                File txt = FileUtil.file(levelTwo, StrUtil.format("{}.txt", levelTwoName));
+                txt.createNewFile();
+                FileUtil.writeUtf8String(levelTwoName, txt);
+            }
+        }
+    }
+
+    private void type02(File file) {
+        for (int i = 0; i < 100; i++) {
+            File tmp = FileUtil.file(file, (i + 1) + ".txt");
+            FileUtil.writeUtf8String((i + 1) + "", tmp);
+        }
     }
 }
