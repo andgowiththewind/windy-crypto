08fdf0fe526eda40473560b32086bafa50fda56d
gust <2414566866@qq.com>
2024-08-28 16:38:10 +0800

    crypto

---
+++ src/main/java/com/gust/cafe/windycrypto/components/AppLock.java
@@ -0,0 +1,45 @@
+package com.gust.cafe.windycrypto.components;
+
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.system.SystemUtil;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileLock;
+
+@Component
+public class AppLock {
+
+    private FileLock fileLock;
+    private FileChannel fileChannel;
+
+    @PostConstruct
+    public void lockFile() throws Exception {
+        File file = FileUtil.file(SystemUtil.getUserInfo().getCurrentDir(), "app.lock");
+        if (!FileUtil.exist(file)) FileUtil.touch(file);
+        //
+        fileChannel = new FileOutputStream(file).getChannel();
+        fileLock = fileChannel.tryLock();
+
+        if (fileLock == null) {
+            throw new IllegalStateException("Unable to acquire lock on app.lock file. Another instance might be running.");
+        }
+
+        System.out.println("Lock acquired on app.lock file.");
+    }
+
+    @PreDestroy
+    public void releaseFileLock() throws Exception {
+        if (fileLock != null) {
+            fileLock.release();
+            System.out.println("Lock released on app.lock file.");
+        }
+        if (fileChannel != null) {
+            fileChannel.close();
+        }
+    }
+}
