a7f8b4691df76a93b3049a33070369ec25a4d384
gust <2414566866@qq.com>
2024-08-29 11:10:59 +0800

    文件名加密

--- src/main/java/com/gust/cafe/windycrypto/constant/CacheConstants.java
+++ src/main/java/com/gust/cafe/windycrypto/constant/CacheConstants.java
@@ -4,9 +4,9 @@ public class CacheConstants {
     // 一级缓存caffeine:接口防抖
     public static final String CAFFEINE_ANTI_SHAKE_LOCK = "caffeineAntiShakeLock";
     // 存放<绝对路径,ID>的map
-    public static final String PATH_ID_MAP = "pathIdMap";
+    public static final String PATH_ID_MAP = "path_id_map";
     // 存放<ID,文件对象>的map
-    public static final String WINDY_MAP = "windyMap";
+    public static final String WINDY_MAP = "windy_map";
     // 加锁,当通过绝对路径获取Windy对象时
     public static final String WINDY_GET_OR_DEFAULT_LOCK = "windyGetOrDefaultLock";
     // 配置更新锁
