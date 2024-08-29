d0a6a47ec3eb38b47fdb24eb1ff13cd199a49349
gust <2414566866@qq.com>
2024-08-29 10:46:09 +0800

    文件名加密

--- windycrypto-web-ui/src/lang/en.js
+++ windycrypto-web-ui/src/lang/en.js
@@ -1,5 +1,5 @@
 export default {
-    i18n_1826858923819405312: 'please input the path',
+    i18n_1826858923819405312: 'LOAD TREE',
     i18n_1826859557616488448: 'please enter the desired top-level directory path and press Enter...',
     i18n_1826861420642439168: 'query',
     i18n_1826862190838288384: 'Unencrypted',
