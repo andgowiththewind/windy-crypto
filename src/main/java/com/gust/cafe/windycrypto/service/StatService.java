ba7bbef6603eb78416269c01222ce93c45997127
gust <2414566866@qq.com>
2024-08-30 15:23:46 +0800

    websocket

--- src/main/java/com/gust/cafe/windycrypto/service/StatService.java
+++ src/main/java/com/gust/cafe/windycrypto/service/StatService.java
@@ -1,14 +1,23 @@
 package com.gust.cafe.windycrypto.service;
 
+import cn.hutool.core.collection.CollectionUtil;
+import cn.hutool.core.convert.Convert;
 import com.gust.cafe.windycrypto.components.RedisSlaveCache;
+import com.gust.cafe.windycrypto.components.WindyLang;
+import com.gust.cafe.windycrypto.constant.CacheConstants;
 import com.gust.cafe.windycrypto.constant.ThreadPoolConstants;
+import com.gust.cafe.windycrypto.dto.core.Windy;
+import com.gust.cafe.windycrypto.enums.WindyStatusEnum;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
 
 
 @Slf4j
@@ -20,8 +29,48 @@ public class StatService {
     @Qualifier(ThreadPoolConstants.STAT)
     private ThreadPoolTaskExecutor statTaskExecutor;
 
-    //
+    /**
+     * 返回两个表格的数据
+     * <p>都使用线程池进行优化</p>
+     *
+     * @param sessionId ws连接的唯一标识
+     * @param ids       第一个表格的id集合
+     */
+    @SneakyThrows
     public void onMessageUpdateTableCache(String sessionId, List<String> ids) {
-        System.out.println();
+        // (1) 条件分页查询的table数据
+        List<Windy> insightTableData = new ArrayList<>();
+        if (CollectionUtil.isNotEmpty(ids)) {
+            List<CompletableFuture<Windy>> futureList = ids.stream()
+                    .map(id -> CompletableFuture.supplyAsync(() -> {
+                        Windy cacheMapValue = redisSlaveCache.getCacheMapValue(CacheConstants.WINDY_MAP, id);
+                        return cacheMapValue;
+                    }, statTaskExecutor))
+                    .collect(Collectors.toList());
+            CompletableFuture<Void> allFutures = CompletableFuture.allOf(futureList.toArray(new CompletableFuture[0]));
+            CompletableFuture<List<Windy>> allResults = allFutures.thenApply(v -> futureList.stream().map(CompletableFuture::join).collect(Collectors.toList()));
+            List<Windy> windyList = allResults.get();
+            windyList.forEach(windy -> Optional.ofNullable(windy).filter(w -> w != null).ifPresent(insightTableData::add));
+        }
+        //
+        // (2) 正在加解密的全部记录
+        List<Windy> processTableData = new ArrayList<>();
+        Map<String, Windy> windyMap = redisSlaveCache.getCacheMap(CacheConstants.WINDY_MAP);
+        Set<String> idSet = windyMap.keySet();
+        if (CollectionUtil.isNotEmpty(idSet)) {
+            List<CompletableFuture<Windy>> futureList = idSet.stream().map(id -> CompletableFuture.supplyAsync(() -> {
+                Windy windy = windyMap.get(id);
+                // 涉及筛选,如果不符合条件,返回null,后续合并结果时根据是否为null进行判断
+                if (windy != null && windy.getCode() != null) {
+                    boolean matchCode = windy.getCode().intValue() == WindyStatusEnum.OUTPUTTING.getCode() || windy.getCode().intValue() == WindyStatusEnum.ALMOST.getCode();
+                    return matchCode ? windy : null;
+                }
+                return null;
+            }, statTaskExecutor)).collect(Collectors.toList());
+            CompletableFuture<Void> allFutures = CompletableFuture.allOf(futureList.toArray(new CompletableFuture[0]));
+            CompletableFuture<List<Windy>> allResults = allFutures.thenApply(v -> futureList.stream().map(CompletableFuture::join).collect(Collectors.toList()));
+            List<Windy> windyList = allResults.get();
+            windyList.forEach(windy -> Optional.ofNullable(windy).filter(w -> w != null).ifPresent(processTableData::add));
+        }
     }
 }
