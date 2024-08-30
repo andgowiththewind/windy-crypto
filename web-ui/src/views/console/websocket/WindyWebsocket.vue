eda554cd0078bc25f9c7779d36919f7f4d55da0c
gust <2414566866@qq.com>
2024-08-30 11:52:13 +0800

    websocket

--- web-ui/src/views/console/websocket/WindyWebsocket.vue
+++ web-ui/src/views/console/websocket/WindyWebsocket.vue
@@ -33,17 +33,17 @@ export default {
     },
     // 检查,如果ws连接不存在则创建
     buildWsConnect() {
-      if (this.cryptoWebSocketVo != null && this.cryptoWebSocketVo != undefined && this.cryptoWebSocketVo.readyState === WebSocket.OPEN) {
-        devConsoleLog('ws连接状态正常,无需重复创建');
+      if (this.wsInstanceVo != null && this.wsInstanceVo != undefined && this.wsInstanceVo.readyState === WebSocket.OPEN) {
+        // devConsoleLog('WS连接状态正常,无需重复创建');
         return true;
       }
       if (!this.wsUrlPrefix) {
-        devConsoleLog('前置条件未满足:wsUrlPrefix为空,无法创建ws连接');
+        devConsoleLog('前置条件未满足:`WS_URL_PREFIX`为空,无法创建WS连接');
+        this.wsUrlPrefixInit();
         return false;
       }
       if (this.wsPromise) {
-        this.wsUrlPrefixInit();
-        devConsoleLog('前置条件未满足:wsPromise存在,有其他线程正在创建ws连接,当前线程终止防止交叉构建');
+        devConsoleLog('前置条件未满足:`WS_PROMISE`存在,有其他线程正在创建WS连接,当前线程终止防止交叉构建');
         return false;
       }
       // 可以构建
@@ -53,33 +53,34 @@ export default {
       // 构建Promise
       this.wsPromise = new Promise((resolve, reject) => {
         try {
-          devConsoleLog(`[${sessionId}]-开始构建ws连接...`);
+          devConsoleLog(`[${sessionId}]-开始构建WS连接...`);
           // 此时是异步的,不会阻塞
           this.wsInstanceVo = new WebSocket(url);
           // 定义future:定义连接成功时的future
           this.wsInstanceVo.onopen = () => {
-            devConsoleLog(`[${sessionId}]-ws连接创建成功`);
+            devConsoleLog(`[${sessionId}]-WS连接创建成功`);
             this.wsSessionId = sessionId;
             resolve(this.wsInstanceVo);
           };
           // 定义future:定义连接失败时的future
           this.wsInstanceVo.onerror = (e) => {
-            devConsoleLog(`[${sessionId}]-ws连接创建失败`, e);
+            devConsoleLog(`[${sessionId}]-WS连接创建失败`, e);
             reject(e);
           };
           // 定义future:定义连接关闭时的future
           this.wsInstanceVo.onclose = (e) => {
-            devConsoleLog(`[${sessionId}]-ws连接关闭`, e);
+            devConsoleLog(`[${sessionId}]-WS连接关闭`, e);
             this.wsPromise = null;
             this.wsSessionId = '';
+            reject(e);
           };
           // 定义future:接收到消息时的future
           this.wsInstanceVo.onmessage = (event) => {
-            devConsoleLog(`[${sessionId}]-ws接收到消息`, event);
+            devConsoleLog(`[${sessionId}]-WS接收到消息`, event);
             this.wsMsgDispatch(event.data);
           };
         } catch (e) {
-          devConsoleLog('ws连接创建失败', e);
+          devConsoleLog('WS连接创建失败', e);
           reject(e);
         }
       });
@@ -90,7 +91,7 @@ export default {
     },
     // ws消息分发
     wsMsgDispatch(msg) {
-      devConsoleLog('ws消息分发', msg);
+      devConsoleLog('WS消息分发', msg);
     },
   },// methods
   watch: {
