a159e023d1fac8af7aa7b3ea474a9c480fad0f33
gust <2414566866@qq.com>
2024-08-31 19:51:51 +0800

    stat

--- src/main/java/com/gust/cafe/windycrypto/job/IoBytesBySecondJob.java
+++ src/main/java/com/gust/cafe/windycrypto/job/IoBytesBySecondJob.java
@@ -24,11 +24,10 @@ public class IoBytesBySecondJob {
         // 删除1分钟前的缓存list
         Collection<String> keys = redisMasterCache.keys(StrUtil.format("{}_*", CacheConstants.LAST_MINUTE_IO));
         keys.stream().forEach(keyStr -> {
-            String[] split = keyStr.split("_");
-            String str = split[1];
+            String str = StrUtil.subAfter(keyStr, StrUtil.format("{}_", CacheConstants.LAST_MINUTE_IO), false);
             DateTime dateTime = DateUtil.parse(str, "yyyyMMddHHmmss");
-            DateTime offsetSecond = DateUtil.offsetSecond(DateUtil.date(), -1 * 60 * 5);
-            // 如果是一分钟前则删除
+            DateTime offsetSecond = DateUtil.offsetSecond(DateUtil.date(), -1 * 60 * 2);
+            // 如果是N分钟前则删除，避免积压
             if (dateTime.before(offsetSecond)) {
                 redisMasterCache.listDelete(keyStr);
                 log.debug("[IO BYTES BY SECOND JOB]-删除缓存:[key={}]", keyStr);
