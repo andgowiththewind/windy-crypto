6b5a469969579c78cf53c904ae5c1fd1b4ab11de
gust <2414566866@qq.com>
2024-09-01 19:37:42 +0800

    exe

--- src/test/java/com/gust/cafe/windycrypto/pkg/PackageAfterTests.java
+++ src/test/java/com/gust/cafe/windycrypto/pkg/PackageAfterTests.java
@@ -1,12 +1,17 @@
 package com.gust.cafe.windycrypto.pkg;
 
 import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.lang.Assert;
 import cn.hutool.core.lang.Console;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.system.SystemUtil;
+import lombok.SneakyThrows;
+import net.lingala.zip4j.ZipFile;
 
 import java.io.File;
 import java.util.Arrays;
+import java.util.Optional;
+
 
 public class PackageAfterTests {
     public static void main(String[] args) {
@@ -19,11 +24,41 @@ public class PackageAfterTests {
     }
 
     private static void extracted() {
-        File from = FileUtil.file(SystemUtil.getUserInfo().getCurrentDir(), "attachments");
-        File to = FileUtil.file(SystemUtil.getUserInfo().getCurrentDir(), "target/attachments");
-        FileUtil.copyContent(from, to, true);
-        Console.log("拷贝成功：{} ===> {}", FileUtil.getAbsolutePath(from), FileUtil.getAbsolutePath(to));
-        //
+        sqlite();
+        redis();
+        exe4j();
+    }
 
+    @SneakyThrows
+    private static void sqlite() {
+        Optional<File> max = Arrays.stream(FileUtil.ls(FileUtil.getAbsolutePath(FileUtil.file(getCurrentDir(), "attachments/db"))))
+                .filter(File::isFile)
+                .filter(f -> StrUtil.startWithIgnoreCase(FileUtil.getName(f), "windy_crypto_") && StrUtil.endWithIgnoreCase(FileUtil.getName(f), ".zip"))
+                .max((f1, f2) -> {
+                    String time1 = StrUtil.subBetween(FileUtil.getName(f1), "windy_crypto_", ".zip");
+                    String time2 = StrUtil.subBetween(FileUtil.getName(f2), "windy_crypto_", ".zip");
+                    return time1.compareTo(time2);
+                });
+        Assert.isTrue(max.isPresent(), "sqlite文件不存在");
+        File sqliteZip = max.get();
+        File toDir = FileUtil.file(getCurrentDir(), "target/attachments/db");
+        new ZipFile(sqliteZip).extractAll(FileUtil.getAbsolutePath(toDir));
+        //
+        File bat = FileUtil.file(getCurrentDir(), "attachments/db/100---实时打包数据库文件.bat");
+        if (FileUtil.exist(bat) && FileUtil.isFile(bat)) {
+            File batCopy = FileUtil.file(getCurrentDir(), "attachments/db", FileUtil.getName(bat));
+            FileUtil.copy(bat, batCopy, true);
+        }
+    }
+
+    private static void redis() {
+    }
+
+    private static void exe4j() {
+    }
+
+
+    private static String getCurrentDir() {
+        return SystemUtil.getUserInfo().getCurrentDir();
     }
 }
