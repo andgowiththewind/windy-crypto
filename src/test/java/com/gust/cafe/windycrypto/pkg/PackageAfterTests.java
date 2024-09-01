9070c966ad32288634955acbf8d401cc1ce67d8d
gust <2414566866@qq.com>
2024-09-01 16:03:22 +0800

    PackageBeforeTests

--- src/test/java/com/gust/cafe/windycrypto/pkg/PackageAfterTests.java
+++ src/test/java/com/gust/cafe/windycrypto/pkg/PackageAfterTests.java
@@ -1,12 +1,21 @@
 package com.gust.cafe.windycrypto.pkg;
 
 import cn.hutool.core.lang.Console;
+import cn.hutool.core.util.StrUtil;
 
 import java.util.Arrays;
 
 public class PackageAfterTests {
     public static void main(String[] args) {
-        Console.error("======================================PackageAfterTests");
-        Arrays.stream(args).forEach(System.out::println);
+        String repeat = StrUtil.repeat("=", 100);
+        String simpleName = PackageAfterTests.class.getSimpleName();
+        //
+        Console.error(StrUtil.format("{} {} {} {}", repeat, simpleName, "start", repeat));
+        extracted();
+        Console.error(StrUtil.format("{} {} {} {}", repeat, simpleName, "end  ", repeat));
+    }
+
+    private static void extracted() {
+
     }
 }
