86ac8edce3626537d31b28cedce79f9abbdc3d86
gust <2414566866@qq.com>
2024-09-01 16:11:45 +0800

    PackageBeforeTests

--- src/test/java/com/gust/cafe/windycrypto/pkg/PackageAfterTests.java
+++ src/test/java/com/gust/cafe/windycrypto/pkg/PackageAfterTests.java
@@ -1,8 +1,11 @@
 package com.gust.cafe.windycrypto.pkg;
 
+import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.lang.Console;
 import cn.hutool.core.util.StrUtil;
+import cn.hutool.system.SystemUtil;
 
+import java.io.File;
 import java.util.Arrays;
 
 public class PackageAfterTests {
@@ -16,6 +19,11 @@ public class PackageAfterTests {
     }
 
     private static void extracted() {
+        File from = FileUtil.file(SystemUtil.getUserInfo().getCurrentDir(), "attachments");
+        File to = FileUtil.file(SystemUtil.getUserInfo().getCurrentDir(), "target/attachments");
+        FileUtil.copyContent(from, to, true);
+        Console.log("拷贝成功：{} ===> {}", FileUtil.getAbsolutePath(from), FileUtil.getAbsolutePath(to));
+        //
 
     }
 }
